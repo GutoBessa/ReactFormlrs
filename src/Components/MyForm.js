cc980ac4b3a9864b62a720964e3ababe763dc107
unknown <luiz-augustosouza@hotmail.com>
2024-07-30 08:19:36 +0100

    Label envolvendo input.

--- src/Components/MyForm.js
+++ src/Components/MyForm.js
@@ -10,6 +10,11 @@ const MyForm = () => {
           <label htmlFor="name">Nome:</label>
           <input type="text" name="name" placeholder="Digite seu nome aqui" />
         </div>
+        {/* 2 - label envolvendo input */}
+        <label>
+          <span>Email:</span>
+          <input type="email" name="email" placeholder="Digite seu email." />
+        </label>
         <input type="submit" value="Enviar" />
       </form>
     </div>
